47ec22a58a7d1e97e779b048726d7a1849d71afd
cj <carljohan.dahlman@virtualstores.se>
2022-01-08 16:25:36 +0100

    updating dependencies

--- Package.swift
+++ Package.swift
@@ -17,7 +17,7 @@ let package = Package(
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
-        .package(url: "https://github.com/virtualstores/ios-foundation.git", from: "0.0.5"),
+        .package(url: "https://github.com/virtualstores/ios-foundation.git", from: "0.0.6"),
         .package(url: "https://github.com/virtualstores/ios-position-kit.git", branch: "fixing-threading-and-tests"),
 
     ],
