957cfa73b1a63f8f559a9039dbf63e0903effb33
HripsimeMartirosyan1990 <96061231+HripsimeMartirosyan1990@users.noreply.github.com>
2022-01-12 11:27:26 +0100

    Made some flow changes for demo app usage (#4)
    * Made some flow changes for demo app usage
    
    * add new TT2PositionManager

--- Sources/VSTT2/VSTT2Config.swift
+++ Sources/VSTT2/VSTT2Config.swift
@@ -1,17 +1,20 @@
 //
-//  File.swift
-//  
+// VSTT2Config
+// VSTT2
 //
-//  Created by Hripsime on 2021-12-21.
-//
+// Created by Hripsime on 2021-12-21.
+// Copyright Virtual Stores - 2021
 
 import Foundation
 import VSFoundation
+import VSPositionKit
 
 struct VSTT2Config: Config {
     func configure(_ injector: Injector) {
+        configureHelpers(injector)
         configureApiEnvironment(injector)
         configureServices(injector)
+        configureManagers(injector)
     }
 
     private func configureApiEnvironment(_ injector: Injector) {
@@ -35,6 +38,18 @@ struct VSTT2Config: Config {
 
         injector.map(MapFenceDataService.self) {
             MapFenceDataService(with: NetworkManager())
+        }
+    }
+
+    private func configureManagers(_ injector: Injector) {
+        injector.map(PositionManager.self) {
+            PositionManager()
+        }
+    }
+    
+    private func configureHelpers(_ injector: Injector) {
+        injector.map(Logger.self) {
+            Logger(verbosity: .debug)
         }
     }
 }
--- Sources/VSTT2/VSTT2Manager.swift
+++ Sources/VSTT2/VSTT2Manager.swift
@@ -11,123 +11,48 @@ import VSFoundation
 import VSPositionKit
 
 final public class VSTT2Manager: VSTT2 {
-    public var stepCountPublisher: CurrentValueSubject<Int, Never> = .init(0)
-    public var positionBundlePublisher: CurrentValueSubject<PositionBundle?, PositionKitError> = .init(nil)
-
+    public var availableStores: CurrentValueSubject<StoresList?, VSTT2Error> = .init(nil)
     // MARK: Private members
     private let context = Context(VSTT2Config())
     private var cancellable = Set<AnyCancellable>()
-    private var publisherCancellable: AnyCancellable?
-    private var positionBundleCancellable: AnyCancellable?
-    private var positionManager: PositionManager?
-
-    @Inject var clientsListService: ClientsListService
+    private var isMapFunctionalityAvailable: Bool
+    private var activeStore: Store?
+    
+    @Inject var positionManager: PositionManager
     @Inject var storesListService: StoresListService
-    @Inject var mapFenceDataService: MapFenceDataService
+    @Inject var tt2PositionManager: TT2PositionManager
 
-    public init() {}
+    public init(with apiUrl: String? = nil, apiKey: String? = nil, clientId: String, isMapAvailable: Bool = true) {
+        self.isMapFunctionalityAvailable = isMapAvailable
 
-    /// Just testing some API calles to be sure that the flow is working
-    /// After will add logic where we need have each API call
-    public func start() throws {
-        positionManager = PositionManager()
-        try positionManager?.start()
-
-        getClients()
-        bindPublishers()
-    }
-
-    public func stop() {
-        positionManager?.stop()
-        publisherCancellable?.cancel()
+        self.getStores(with: clientId)
     }
 
     public func setBackgroundAccess(isActive: Bool) {
-        positionManager?.setBackgroundAccess(isActive: isActive)
+        positionManager.setBackgroundAccess(isActive: isActive)
     }
-
-    private func bindPublishers() {
-        publisherCancellable = positionManager?.stepCountPublisher
-            .sink { [weak self] error in
-                self?.stepCountPublisher.send(completion: error)
-            } receiveValue: { [weak self]  data in
-                self?.stepCountPublisher.send(data)
-            }
-        
-        positionBundleCancellable = positionManager?.positionPublisher
-            .sink { [weak self] error in
-                self?.positionBundlePublisher.send(completion: error)
-            } receiveValue: { [weak self]  positionBundle in
-                self?.positionBundlePublisher.send(positionBundle)
-//                print(positionBundle)
-            }
-    }
-
-    deinit {
-        stop()
+    
+    public func initiateStore(store: Store) {
+        self.activeStore = store
+        tt2PositionManager.configureStoreData(for: store)
     }
 }
 
 // MARK: API calles
 private extension VSTT2Manager {
-    private func getClients() {
-        let parameters = ClientsListParameters()
-        clientsListService
-            .call(with: parameters)
-            .sink(receiveCompletion: { (completion) in
-                switch completion {
-                case .finished:
-                    break
-                case .failure(let error):
-                    print(error)
-                }
-            }, receiveValue: { [weak self] (data) in
-                guard let clientId = data.clients.first?.clientId else { return }
-
-                self?.getStores(with: clientId)
-            }).store(in: &cancellable)
-    }
-
     private func getStores(with clientId: String) {
         let parameters = StoresListParameters(clientId: clientId)
         storesListService
             .call(with: parameters)
-            .sink(receiveCompletion: { (completion) in
+            .sink(receiveCompletion: { [weak self] (completion) in
                 switch completion {
                 case .finished:
                     break
-                case .failure(let error):
-                    print(error)
+                case .failure:
+                    self?.availableStores.send(completion: .failure(VSTT2Error.noAvailableStores))
                 }
             }, receiveValue: { [weak self] (data) in
-                // For now just using 4th element as we know it's the one we can test
-                guard let url = data.stores[4].rtlsOptions.mapFenceUrl else { return }
-
-                self?.getMapFenceData(with: url)
-            }).store(in: &cancellable)
-    }
-
-    private func getMapFenceData(with url: String) {
-        let parameters = MapFenceDataParameters(url: url)
-        mapFenceDataService
-            .call(with: parameters)
-            .sink(receiveCompletion: { (completion) in
-                switch completion {
-                case .finished:
-                    break
-                case .failure(let error):
-                    print(error)
-                }
-            }, receiveValue: { [weak self] (data) in
-                guard let self = self else { return }
-
-                do {
-                    try self.positionManager?.setupMapFence(with: data)
-                    self.positionManager?.startNavigation(with: 180, xPosition: 4.570476, yPosition: 2.094637)
-
-                } catch {
-                    // Add error handling logic when MapFence setup failed
-                }
+                self?.availableStores.send(data)
             }).store(in: &cancellable)
     }
 }
