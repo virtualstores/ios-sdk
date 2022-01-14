8601c7fe18f055d8b659a818dd627fb1adbf5aec
Hripsime <hripsime.martirosyan@shortcut.no>
2022-01-14 14:08:14 +0100

    fix file name

--- Sources/VSTT2/Api/Servicing/UploadPositions/UploadPositionsService.swift
+++ Sources/VSTT2/Api/Servicing/UploadPositions/UploadPositionsService.swift
@@ -1,9 +1,9 @@
 //
-//  File.swift
-//  
+// UploadPositionsService
+// VSTT2
 //
-//  Created by Hripsime on 2022-01-13.
-//
+// Created by Hripsime on 2022-01-13.
+// Copyright Virtual Stores - 2021
 
 import Foundation
 import Combine
