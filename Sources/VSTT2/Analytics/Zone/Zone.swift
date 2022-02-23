6ca9faa14cb35704848fd94537253f954c496519
Hripsime <hripsime.martirosyan@shortcut.no>
2022-02-23 15:40:16 +0100

    added Tree and Zone for creating zones

---
+++ Sources/VSTT2/Analytics/Zone/Zone.swift
@@ -0,0 +1,110 @@
+//
+// Zone
+// VSTT2
+//
+// Created by Hripsime on 2022-02-22
+// Copyright Virtual Stores - 2022
+
+import Foundation
+import CoreGraphics
+import UIKit
+import VSFoundation
+
+public class Zone: Equatable {
+    public let name: String
+    public let id: String
+    public let floorLevel: Int
+    public var color: UIColor = .white
+    public var image: UIImage?
+    public internal(set) var polygon: [CGPoint]
+    public internal(set) var parent: Zone?
+    public internal(set) var children: Dictionary<String, Zone>
+    public internal(set) var searchTerms: [String]
+    internal var navigationPoint: CGPoint?
+    private let converter: BaseCoordinateConverter
+
+
+    init(id:String, name: String, polygon: [CGPoint] = [], navigationPoint: CGPoint? = nil, parent: Zone? = nil, children: Dictionary<String, Zone> = [:], searchTerms: [String] = [], floorLevel: Int, converter: BaseCoordinateConverter) {
+        self.id = id
+        self.name = name
+        self.polygon = polygon
+        self.navigationPoint = navigationPoint
+        self.parent = parent
+        self.children = children
+        self.searchTerms = searchTerms
+        self.floorLevel = floorLevel
+        self.converter = converter
+    }
+    
+    var points: [CGPoint] {
+        var points: [CGPoint] = []
+        for point in polygon {
+            points.append(CGPoint(x: converter.convertFromMapCoordinateToMeters(input: point.x), y: converter.convertFromMapCoordinateToMeters(input: point.y)))
+        }
+        return points
+    }
+    
+    func addChild(child: Zone) {
+        let zone = Zone(id: child.id, name: child.name, polygon: child.polygon, navigationPoint: child.navigationPoint, parent: self, searchTerms: child.searchTerms, floorLevel: child.floorLevel, converter: converter)
+        self.children[child.name] = zone
+    }
+    
+    func getChildren() -> [Zone]? {
+        var list: [Zone] = []
+        
+        list.append(self)
+        
+        for key in self.children.keys {
+            if let zones = self.children[key]?.getChildren() {
+                list += zones
+            }
+        }
+        
+        return !list.isEmpty ? list : nil
+    }
+    
+    func recursivePrint(_ padding: String) {
+        if self.parent != nil {
+            Logger.init(verbosity: .debug).log(message: "\(padding)\(self.name)")
+        } else {
+            Logger.init(verbosity: .debug).log(message: padding)
+            Logger.init(verbosity: .debug).log(message: self.name)
+        }
+        
+        for key in self.children.keys {
+            self.children[key]?.recursivePrint(padding + "    ")
+        }
+    }
+    
+    func recursiveSearch(_ searchString: String) -> [Zone]? {
+        var list: [Zone] = []
+        
+        var search: String = self.name
+        
+        for string in searchTerms {
+            search = search + ":" + string
+        }
+        
+        if search.lowercased().contains(searchString.lowercased()) {
+            list.append(self)
+        }
+        
+        for key in self.children.keys {
+            if let zones = self.children[key]?.recursiveSearch(searchString) {
+                list += zones
+            }
+        }
+        
+        return !list.isEmpty ? list : nil
+    }
+
+    /// Uses name parameter for comparison
+    public static func == (lhs: Zone, rhs: Zone) -> Bool {
+        lhs.name == rhs.name
+    }
+    
+    /// Uses name parameter for comparison
+    public static func < (lhs: Zone, rhs: Zone) -> Bool {
+        lhs.name < rhs.name
+    }
+}
