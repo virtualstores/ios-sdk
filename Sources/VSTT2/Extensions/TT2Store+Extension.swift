551f3d94c75d4406ba0051002cd14e0151542169
Hripsime <hripsime.martirosyan@shortcut.no>
2022-03-07 09:25:24 +0100

    added TT2Store extension

---
+++ Sources/VSTT2/Extensions/TT2Store+Extension.swift
@@ -0,0 +1,15 @@
+//
+// Store
+// VSTT2
+//
+// Created by Hripsime on 2021-12-20
+// Copyright Virtual Stores - 2021
+
+
+import Foundation
+
+public extension Store {
+    func toTT2Store() -> TT2Store {
+        return TT2Store(id: self.id, name: self.name, address: self.address, latitude: self.latitude, longitude: self.longitude, active: self.active, startScanLocations: self.startCodes, stopScanLocations: self.stopCodes)
+    }
+}
