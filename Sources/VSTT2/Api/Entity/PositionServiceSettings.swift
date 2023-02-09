e669f8ad53dc6272287728707a1b0cd06055d6a1
Théodore Roos <25298135+TheodoreRoos@users.noreply.github.com>
2023-02-09 15:17:23 +0100

    Parsing custom values in PositionServiceSettings

--- Sources/VSTT2/Api/Entity/PositionServiceSettings.swift
+++ Sources/VSTT2/Api/Entity/PositionServiceSettings.swift
@@ -11,4 +11,11 @@ public struct PositionServiceSettings: Codable {
   public let useML: Bool
   public let useCoefficientOptimizer: Bool
   public let useDriftCompensator: Bool
+  public let boolValues: [String: Bool]?
+  public let stringValues: [String: String]?
+  public let stringArrayValues: [String: [String]]?
+  public let intValues: [String: Int]?
+  public let intArrayValues: [String: [Int]]?
+  public let floatValues: [String: Double]?
+  public let floatArrayValues: [String: [Double]]?
 }
