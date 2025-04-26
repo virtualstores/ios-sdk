43cf75c74bc17f242b993b3ce42c5199c9944cad
Théodore Roos <25298135+TheodoreRoos@users.noreply.github.com>
2025-04-26 16:08:22 +0200

    Swiftified

--- Sources/VSTT2/UseCases/SaveAnalyticsToFileUseCase.swift
+++ Sources/VSTT2/UseCases/SaveAnalyticsToFileUseCase.swift
@@ -6,7 +6,6 @@
 import Foundation
 import VSFoundation
 
-
 class CreateOfflineVisitDirectoryUseCase {
   @Inject var repository: IAnalyticsRepository
 
@@ -24,7 +23,6 @@ class CreateOfflineVisitDirectoryUseCase {
 }
 
 class CreateOfflineVisitDirectoryBussiness {
-
   func invoke(visitId: Int64, sessionId: Int64, timestamp: String) -> URL? {
     /// Create a directory: 2025-04-25T10-50-34_visitId_sessionId
     /// Set directory in repoisitory
@@ -56,80 +54,65 @@ class CreateOfflineVisitDirectoryBussiness {
   }
 }
 
-
-
 class SaveAnalyticsToFileUseCase {
   @Inject var persistence: PersistenceManager
   @Inject var repository: IAnalyticsRepository
 
+  let business = SaveAnalyticsToFileBusiness()
   func invoke(){
     /// Extract current visit- and session Id
     /// Combine all visit geoPositions into one dictionary
     /// Convert to Json file
     /// Save Json file to directory found in the repository
-
-    let bussiness = SaveAnalyticsToFileBussiness()
-
     if let visitId = repository.activeVisitId {
-      let bundledGeoPositions = bussiness.bundleGeoPositionsParameters(geoPositions: persistence.getGeoPositions())
-      let visitGeoPositions = bussiness.createVisitGeoPositions(geoPositionsBundles: bundledGeoPositions, visitId: visitId)
+      let bundledGeoPositions = business.bundleGeoPositionsParameters(geoPositions: persistence.getGeoPositions())
+      let visitGeoPositions = business.createVisitGeoPositions(geoPositionsBundles: bundledGeoPositions, visitId: visitId)
       if let visitGeoPositions = visitGeoPositions {
         // todo get directoryURL from repository to save the data
         if let directoryURL = repository.directoryURLS[visitId]{
-          bussiness.writeToFile(data: visitGeoPositions, directoryUrl: directoryURL)
+          business.writeToFile(data: visitGeoPositions, directoryUrl: directoryURL)
         }
       }
     }
   }
 }
 
-
-class SaveAnalyticsToFileBussiness {
-
+class SaveAnalyticsToFileBusiness {
   func writeToFile(data: VisitGeoPositions, directoryUrl: URL) {
-
-    if let jsonData = try? JSONEncoder().encode(data) {
-
-      do {
-        let fileUrl = directoryUrl.appendingPathComponent("geoPositions.json")
-        try jsonData.write(to: fileUrl)
-
-      } catch {
-
-      }
+    do {
+      let jsonData = try JSONEncoder().encode(data)
+      let fileUrl = directoryUrl.appendingPathComponent("geoPositions.json")
+      try jsonData.write(to: fileUrl)
+    } catch {
+      print(#function, "Couldn't encode data", error)
     }
   }
 
-
   func createVisitGeoPositions(geoPositionsBundles: [Int64: [String: [RecordedPositionLngLat]]], visitId: Int64) -> VisitGeoPositions? {
-    if geoPositionsBundles[visitId] == nil {
-      return nil
-    } else {
-      var visitGeoPositions = VisitGeoPositions(visitId: visitId, sessionId: 0, tt2Positions: [], gpsPositions: [])
-      geoPositionsBundles[visitId]?.forEach { key, value in
-        if key == "full-gps" {
-          visitGeoPositions.gpsPositions.append(contentsOf: value)
-        } else if key == "full-tt2" {
-          visitGeoPositions.tt2Positions.append(contentsOf: value)
-        }
+    guard geoPositionsBundles[visitId] != nil else { return nil }
+    var visitGeoPositions = VisitGeoPositions(visitId: visitId, sessionId: 0, tt2Positions: [], gpsPositions: [])
+    geoPositionsBundles[visitId]?.forEach { (key, value) in
+      if key == UploadGeoPositionsParameters.TypeEnum.fullGPS.rawValue {
+        visitGeoPositions.gpsPositions.append(contentsOf: value)
+      } else if key == UploadGeoPositionsParameters.TypeEnum.fullVPSMl.rawValue {
+        visitGeoPositions.tt2Positions.append(contentsOf: value)
       }
-      return visitGeoPositions
     }
+    return visitGeoPositions
   }
 
   func bundleGeoPositionsParameters(geoPositions: [UploadGeoPositionsParameters]) -> [Int64: [String: [RecordedPositionLngLat]]]  {
-
     var visitGeoPositions: [Int64: [String: [RecordedPositionLngLat]]] = [:]
 
-    geoPositions.forEach { part in
-      if visitGeoPositions[part.visitId] == nil {
-        visitGeoPositions[part.visitId] = part.positions
+    geoPositions.forEach { (params) in
+      if visitGeoPositions[params.visitId] == nil {
+        visitGeoPositions[params.visitId] = params.positions
       } else {
-        part.positions.forEach { key, value in
-          if visitGeoPositions[part.visitId]?[key] == nil {
-            visitGeoPositions[part.visitId]?[key] = value
+        params.positions.forEach { (key, value) in
+          if visitGeoPositions[params.visitId]?[key] == nil {
+            visitGeoPositions[params.visitId]?[key] = value
           } else {
-            visitGeoPositions[part.visitId]?[key]?.append(contentsOf: value)
+            visitGeoPositions[params.visitId]?[key]?.append(contentsOf: value)
           }
         }
       }
@@ -138,7 +121,6 @@ class SaveAnalyticsToFileBussiness {
     return visitGeoPositions
   }
 }
-
 
 struct VisitGeoPositions: Codable {
   var visitId: Int64
