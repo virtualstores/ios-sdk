fdfbc95c4ab2cce863296e432a3bea32f8427c58
Théodore Roos <25298135+TheodoreRoos@users.noreply.github.com>
2025-07-24 15:30:17 +0200

    Fixed compile error for older Xcode versions

--- Package.swift
+++ Package.swift
@@ -17,7 +17,7 @@ let package = Package(
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
-        .package(url: "https://github.com/virtualstores/ios-foundation.git", .exact("1.8.0")),
+        .package(url: "https://github.com/virtualstores/ios-foundation.git", .exact("1.8.1")),
         .package(url: "https://github.com/virtualstores/ios-position-kit.git", .exact("1.37.0")),
         .package(url: "https://github.com/stephencelis/SQLite.swift.git", .exact("0.13.1")),
         .package(url: "https://github.com/aws-amplify/aws-sdk-ios-spm.git", .exact("2.27.4")),
