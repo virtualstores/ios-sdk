c70d2d6e4653a8613734c40e2cddf5bd3fc7abee
Théodore Roos <25298135+TheodoreRoos@users.noreply.github.com>
2025-11-21 14:26:22 +0100

    [Feature] VPS Update
    * Updated VPS to latest version

--- Sources/VSTT2/TT2.swift
+++ Sources/VSTT2/TT2.swift
@@ -35,7 +35,7 @@ final public class TT2: ITT2 {
     // Only for testing purpose of floorchange. Will be removed once green lighted
     public var floorChangePublisher: CurrentValueSubject<String?, Never> = .init(nil)
 
-    static let version = "2.14.0"
+    static let version = "2.14.1"
 
     // MARK: Private members
     private let tag: String = "TT2"
